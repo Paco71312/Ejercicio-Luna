a88189d9e182ef7f417e3130d98068eaea30456c
Paco71312 <ing.josearjona@gmail.com>
2022-09-20 21:43:15 +0000

    [ADD] Basic Module

---
+++ Odoo_luna/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name':'Odoo Luna',
+    'summary':"""Esta es uan app que ayudar a Odoo a llegar a la luna""",
+    'description':""" App para la Luna de Odoo
+    -Nave Espacial
+    -Tripulacion
+    """,
+    'license':'LGPL-3',
+    'category': 'Training',
+    'depends':['base'],
+    'data'=[
+        
+    ],
+    'version':'0.1',
+    'demo'=[
+        
+    ],
+    'author'='Paco',
+}
