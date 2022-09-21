47c00dbd0742eac57fbbca4f8339996445f0b2de
Paco71312 <ing.josearjona@gmail.com>
2022-09-21 05:41:52 +0000

    [ADD] Basic Module

---
+++ odoo_luna/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,21 @@
+{
+    'name':'Odoo Luna',
+    'summary':"""Esta es uan app que ayudar a Odoo a llegar a la luna""",
+    'description':""" App para la Luna de Odoo
+    -Nave Espacial
+    -Tripulacion
+    """,
+    'license':'LGPL-3',
+    'author':'Paco',
+    'version':'0.1',
+    'category': 'Training',
+    'depends': ['base'],
+    'data':[
+        
+    ],
+    
+    'demo':[
+        
+    ],
+    
+}
---
+++ odoo_luna/models/__init__.py
@@ -0,0 +1,2 @@
+#se abre un nuevo init para que se pueda acceder directamente al modulo sin problemas, tambien aqui se vincula con el exterior del modulo 
+from . import nave
