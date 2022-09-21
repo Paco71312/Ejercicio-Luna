7517831e6cd8d131cade21d9f936f7178bd9da4f
Paco71312 <ing.josearjona@gmail.com>
2022-09-21 05:11:08 +0000

    [ADD] Basic Module

--- Odoo_luna/models/.ipynb_checkpoints/nave-checkpoint.py
+++ Odoo_luna/models/.ipynb_checkpoints/nave-checkpoint.py
@@ -5,6 +5,6 @@ class Nave(models.Model):
     _description='Es una nave espacial'
     _rec_name:'spacial car'
         
-        name=fields.Char(string='Nombre de la nave',required='True')
+        #name=fields.Char(string='Nombre de la nave',required='True')
         #dimensiones=fields.Char(string='Dimensiones de la Nave')
         #tipo_de_combustible=fileds.Selection(string='Tipo de combustible',selection= [('gasolina','Gasolina'),('turbosina','Turbosina')], required='True',copy='False')
