27b31dc8657a3f1e0b8c2322d837a24b75096658
Paco71312 <ing.josearjona@gmail.com>
2022-09-21 01:07:02 +0000

    [ADD] Basic Module

---
+++ Odoo_luna/__init__.py
@@ -0,0 +1,2 @@
+# se importan los archivos a los cuales se deben de ingresar para que nuestor modlo funcione, en este caso seria el de modulos
+from . import models
---
+++ Odoo_luna/models/.ipynb_checkpoints/nave-checkpoint.py
@@ -0,0 +1,6 @@
+from odoo import models, fields, api
+
+class Nave(models.Model):
+    _name='nave.espacial'
+    _description='Es una nave espacial'
+    _rec_name:'spacial car '
