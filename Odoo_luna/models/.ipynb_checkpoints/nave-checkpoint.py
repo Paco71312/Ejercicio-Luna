d9a2e646ba7c60984e0f10e9b68fd3d07e57285f
Paco71312 <ing.josearjona@gmail.com>
2022-09-23 17:19:23 +0000

    [ADD] Basic Module

--- Odoo_luna/models/.ipynb_checkpoints/nave-checkpoint.py
+++ Odoo_luna/models/.ipynb_checkpoints/nave-checkpoint.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Nave(models.Model):
     _name='nave.espacial'
@@ -12,3 +13,12 @@ class Nave(models.Model):
     numero_de_pasajeros=fields.Integer(string='Numero de pasajeros',required='True')
     estatus_de_la_nave=fields.Selection(string='estatus de la nave', selection= [('Activa','activa'),('Retirado','retirado')],copy='False')
     active=fields.Boolean(string='Ative',default=True)
+    
+    ancho=fields.Float(string='Ancho')
+    largo=fields.Float(string='Largo')
+    
+    @api.constrains('ancho', 'largo')
+    def _checar_ancho(self):
+        if self.ancho > self.largo or self.ancho == self.largo:
+            raise UserError(f' SIEMPRE: El ancho {self.ancho} debe ser menor, tampoco igual que el largo {self.largo}')
+            
